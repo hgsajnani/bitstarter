77f363766081624e3b2587d348fbba159b2aefe9
hgsajnani <hgsajnani@gmail.com>
2013-07-05 09:35:09 +0400

    index added

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+var path= require('path');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 4');
+	var filePath = path.join(__dirname, 'index.html');
+    var stat = fs.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
 });
 
 var port = process.env.PORT || 5000;
